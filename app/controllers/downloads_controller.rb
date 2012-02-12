7cf5babf6b391e8c862e00774c6708371a80a73f
Chris <cleesmith2006@gmail.com>
2012-02-11 19:52:46 -0500

    work on securely downloading PDFs

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,9 +1,9 @@
 class DownloadsController < ApplicationController
-  # before_filter :authenticate_user!
-  # before_filter :ensure_user_is_setup
-  # 
-  # respond_to :html
-  # respond_to :pdf, only: [:index, :show]
+  before_filter :authenticate_user!
+  before_filter :ensure_user_is_setup
+  
+  respond_to :html
+  respond_to :pdf, only: [:index, :show]
 
   def index
     render text: 'hello from DownloadsController :-)'
@@ -13,9 +13,13 @@ class DownloadsController < ApplicationController
     # use params to get the filename and content_type
     filename = "snorby_event.pdf"
     content_type = "application/pdf"
+    # use this to put the burden of downloading a file on rails, which will block while doing so:
+    # send_file('/home/cleesmith/apps/osprotect/shared/reports/snorby_event.pdf')
+
+    # use this to let nginx handle downloading the file, which frees up rails to handle the next user:
     head( :x_accel_redirect => '/private_files/' + filename,
           :content_type => content_type,
           :content_disposition => "attachment; filename=\"#{filename}\"")
-    # send_file('/home/cleesmith/apps/osprotect/shared/reports/snorby_event.pdf')
+Rails.logger.info "\n response.headers=#{response.headers.inspect}\n"
   end
 end
