c8036a2d48d2e145cd23c0bed0e8ca24e5e3681d
Chris <cleesmith2006@gmail.com>
2012-02-15 02:28:51 -0500

    work on pdfs

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -12,7 +12,7 @@ class PdfsController < ApplicationController
     if Rails.env.production?
       # note: '/private_files/' is aliased to '/home/cleesmith/apps/osprotect/shared/reports'
       #       see details in /etc/nginx/nginx.conf
-      # use 'head()' to let nginx handle downloading the file, which frees up rails to handle the next user:
+      # use 'head()' to let nginx handle downloading the file, which frees up rails to handle other requests:
       head(x_accel_redirect: "/private_files/#{current_user.id}/" + pdf.file_name,
            content_type: content_type,
            content_disposition: "attachment; filename=\"#{pdf.file_name}\"")
@@ -26,6 +26,29 @@ class PdfsController < ApplicationController
     pdf = current_user.pdfs.find(params[:id])
     pdf.destroy
     # FIXME if record delete then don't forget to delete the actual pdf file ?
+    # def flush_deletes #:nodoc:
+    #   @queued_for_delete.each do |path|
+    #     begin
+    #       log("deleting #{path}")
+    #       FileUtils.rm(path) if File.exist?(path)
+    #     rescue Errno::ENOENT => e
+    #       # ignore file-not-found, let everything else pass
+    #     end
+    #     begin
+    #       while(true)
+    #         path = File.dirname(path)
+    #         FileUtils.rmdir(path)
+    #         break if File.exists?(path) # Ruby 1.9.2 does not raise if the removal failed.
+    #       end
+    #     rescue Errno::EEXIST, Errno::ENOTEMPTY, Errno::ENOENT, Errno::EINVAL, Errno::ENOTDIR, Errno::EACCES
+    #       # Stop trying to remove parent directories
+    #     rescue SystemCallError => e
+    #       log("There was an unexpected error while deleting directories: #{e.class}")
+    #       # Ignore it
+    #     end
+    #   end
+    #   @queued_for_delete = []
+    # end
     redirect_to pdfs_url
   end
 end
