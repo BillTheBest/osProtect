a9957a53c5f218dd22fbbad7f7197e53e21a33d7
Chris <cleesmith2006@gmail.com>
2012-04-12 16:57:18 -0400

    deleted config/initializers/columnize.rb and replaced with lib/osprotect/array.rb as the Array class is used in other parts of the app where this new method is not required, so use require 'osprotect/array' where needed

--- lib/osprotect/array.rb
+++ lib/osprotect/array.rb
@@ -1,3 +1,5 @@
+# use require "osprotect/array" where this new method is needed
+# see: app/pdf_generators/events_pdf.rb for an example
 class Array
   def columnize args = { :columns => 1, :offset => 0 }
     column = []
